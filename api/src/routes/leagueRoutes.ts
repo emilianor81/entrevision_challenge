83a3a34718aa3e0c623ff1a1dec39c6fe96a5883
Emiliano Rodriguez <emilianor81@gmail.com>
2024-07-05 13:26:57 +0200

    Delete unused endpointe

--- api/src/routes/leagueRoutes.ts
+++ api/src/routes/leagueRoutes.ts
@@ -1,20 +1,16 @@
 import { Router } from 'express';
 import {
   importLeagueController,
-/*   importAllLeaguesController,
- */  getTeamsController,
+  getTeamsController,
   getPlayersController
 } from '../controllers/leagueController';
 
 const router = Router();
 //Todo: SWAGGER, hacer un archivo de swagger con los doc de endpoint
-router.post('/index', (req, res) => {
-  console.log('ENTRO AL INDEX!!!!');
-  res.send('Index endpoint');
-});
-
+//Todo: mejorar el README!!! SI O SIII!!!
 router.post('/import-league', importLeagueController);
 router.get('/teams', getTeamsController);
 router.get('/players', getPlayersController);
+
 
 export { router as leagueRoutes };
