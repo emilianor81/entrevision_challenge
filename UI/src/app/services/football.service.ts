e2f9d416aa482631e80c9c265f2ccc906983c346
Emiliano Rodriguez <emilianor81@gmail.com>
2024-07-05 15:36:40 +0200

    Change apiUrl on frontEnd

--- UI/src/app/services/football.service.ts
+++ UI/src/app/services/football.service.ts
@@ -6,15 +6,15 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class FootballService {
-  private apiUrl = 'http://localhost:3000'; // Ajusta esto según la URL de tu API
+  private apiUrl = 'http://localhost:3000/api';
 
   constructor(private http: HttpClient) { }
 
   getTeams(leagueCode: string): Observable<any> {
-    return this.http.get(`${this.apiUrl}/api/teams?leagueCode=${leagueCode}`);
+    return this.http.get(`${this.apiUrl}/teams?leagueCode=${leagueCode}`);
   }
 
   getPlayers(teamId: number): Observable<any> {
-    return this.http.get(`${this.apiUrl}/api/players?teamId=${teamId}`);
+    return this.http.get(`${this.apiUrl}/players?teamId=${teamId}`);
   }
 }
