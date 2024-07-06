426063b9090cb51c251772fdcc007669ec8f937e
Emiliano Rodriguez <emilianor81@gmail.com>
2024-07-06 10:50:25 +0200

    Add validation to input and css styles on button LoadTeams when is disabled & add sweetAlert when a error happens

--- UI/src/app/components/team-list/team-list.component.ts
+++ UI/src/app/components/team-list/team-list.component.ts
@@ -3,27 +3,48 @@ import { FootballService } from '../../services/football.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-team-list',
   standalone: true,
   imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './team-list.component.html',
+  styleUrls: ['./team-list.component.css']  
 })
 export class TeamListComponent implements OnInit {
   leagueCode: string = '';
   teams: any[] = [];
   filteredTeams: any[] = [];
   filter: string = '';
+  isValidInput: boolean = false;
 
   constructor(private footballService: FootballService) { }
 
   ngOnInit(): void {}
 
+  validateInput() {
+    this.isValidInput = this.leagueCode.length >= 2;
+  }
+
   loadTeams() {
     this.footballService.getTeams(this.leagueCode).subscribe(data => {
-      this.teams = data;
-      this.filteredTeams = data;
+      if (data.length === 0) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'No league found with the entered code.'
+        });
+      } else {
+        this.teams = data;
+        this.filteredTeams = data;
+      }
+    }, error => {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'An error occurred while fetching the teams.'
+      });
     });
   }
 
